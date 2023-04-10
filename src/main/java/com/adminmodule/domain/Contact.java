f84013d101a10e0d051c5990ade699c421ae088c
Wende-Kebe <wendmagegn.b@gmail.com>
2023-04-10 02:05:17 -0500

    Admin Module Structure initiated

--- src/main/java/com/adminmodule/domain/Contact.java
+++
@@ -1,4 +0,0 @@
-package com.adminmodule.domain;
-
-public class Contact {
-}
--- src/main/java/com/adminmodule/domain/Customer.java
+++ src/main/java/com/adminmodule/domain/Customer.java
@@ -1,4 +1,30 @@
 package com.adminmodule.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Customer {
+
+    @Id
+    private int customerId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Address address;
+    private String username;
+    private String password;
+    private Role role;
+
+
+
 }
