9792df1656411c6a0396ee1203fbacdc0ee81fcf
Navruzbek <navruzbek.noraliev@gmail.com>
2023-04-11 17:33:17 -0500

    added essential modules and readme file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
