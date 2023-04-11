5f7bf7d6fda92ecd8f495a351973fc4a6e196182
saioufe <saioufeyoutube@yahoo.com>
2023-04-11 14:25:10 -0500

    Init

--- src/main/java/com/example/payment/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/example/payment/paymentservice/PaymentServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentServiceApplication {
-
+// new
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
