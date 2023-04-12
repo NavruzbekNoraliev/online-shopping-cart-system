49c4021150ec6a4055e9b3a9563dd8d906852912
arda009 <arda.agcal@gmail.com>
2023-04-12 16:22:36 -0500

    first

---
+++ src/test/java/com/example/reports/ReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
