e7b5452ce778a8936b2e318ac1a11b666292276c
saioufe <saioufeyoutube@yahoo.com>
2023-04-13 15:19:54 -0500

    Init

---
+++ src/main/java/com/example/bank/Bank_Service/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank.Bank_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank/Bank_Service/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank.Bank_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
