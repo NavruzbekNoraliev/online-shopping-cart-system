5c54d601833831172f0144cf17d734b3d0745dd4
saioufe <saioufeyoutube@yahoo.com>
2023-04-22 22:03:15 -0500

    Payment Service

--- src/main/java/com/example/payment/paymentservice/service/CardService.java
+++ src/main/java/com/example/payment/paymentservice/service/CardService.java
@@ -38,8 +38,66 @@ public class CardService {
 
 
     public ResponseEntity addCard(CardEntity cardEntity) {
+
+        if(cardEntity.getCardNumber() == null || cardEntity.getCardNumber().isEmpty()) {
+            // return error response with message indicating card number is required
+            return ResponseEntity.badRequest().body("Card number is required");
+        }
+
+        if(cardEntity.getCardNumber().length() != 16 ) {
+            // return error response with message indicating card number is required
+            return ResponseEntity.badRequest().body("Card number must be 16 digits");
+        }
+
+        if(cardEntity.getNameOnCard() == null || cardEntity.getNameOnCard().isEmpty()) {
+            // return error response with message indicating name on card is required
+            return ResponseEntity.badRequest().body("Name on card is required");
+        }
+
+        if(cardEntity.getCvv() == null || cardEntity.getCvv() < 100 || cardEntity.getCvv() > 999) {
+            // return error response with message indicating cvv must be 3 digits
+            return ResponseEntity.badRequest().body("CVV must be 3 digits");
+        }
+
+        if(cardEntity.getIssuedValue() == null || cardEntity.getIssuedValue().compareTo(BigDecimal.ZERO) <= 0) {
+            // return error response with message indicating issued value must be greater than 0
+            return ResponseEntity.badRequest().body("Issued value must be greater than 0");
+        }
+
+        if(cardEntity.getCurrentValue() == null || cardEntity.getCurrentValue().compareTo(BigDecimal.ZERO) < 0) {
+            // return error response with message indicating current value must be greater than or equal to 0
+            return ResponseEntity.badRequest().body("Current value must be greater than or equal to 0");
+        }
+
+        if(cardEntity.getOperationMode() == null || cardEntity.getOperationMode().isEmpty()) {
+            // return error response with message indicating operation mode is required
+            return ResponseEntity.badRequest().body("Operation mode is required");
+        }
+
+        if(cardEntity.getExpYear() == null || cardEntity.getExpYear().isEmpty()) {
+            // return error response with message indicating expiration year is required
+            return ResponseEntity.badRequest().body("Expiration year is required");
+        }
+
+        if(cardEntity.getExpYear().length() != 2) {
+            // return error response with message indicating expiration year is required
+            return ResponseEntity.badRequest().body("Expiration year Must be 3 digits only");
+        }
+
+        if(cardEntity.getExpMonth().length() != 2) {
+            // return error response with message indicating expiration year is required
+            return ResponseEntity.badRequest().body("Expiration Month Must be 3 digits only");
+        }
+
+        if(cardEntity.getExpMonth() == null || cardEntity.getExpMonth().isEmpty()) {
+            // return error response with message indicating expiration month is required
+            return ResponseEntity.badRequest().body("Expiration month is required");
+        }
+
         CardEntity optCard = cardRepository.save(cardEntity);
-        return ResponseEntity.ok("card has been added successfully, CARD ID : " + optCard.getCardId());
+        return ResponseEntity.ok("Card has been added successfully, CARD ID: " + optCard.getCardId());
+//        CardEntity optCard = cardRepository.save(cardEntity);
+//        return ResponseEntity.ok("card has been added successfully, CARD ID : " + optCard.getCardId());
     }
 
 
