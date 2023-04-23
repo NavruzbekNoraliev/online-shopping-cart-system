473ca9c0f9a2d9fe69393472811a3d23aa203611
saioufe <saioufeyoutube@yahoo.com>
2023-04-23 15:02:21 -0500

    Bank Service

--- src/main/java/com/example/bank/Bank_Service/KafkaConsumer.java
+++ src/main/java/com/example/bank/Bank_Service/KafkaConsumer.java
@@ -12,18 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
-    @KafkaListener(topics = "transactions")
-    public void consume(String message){
-        System.out.println("Kafka Consumer\n"+message);
-        ObjectMapper om = new ObjectMapper();
-        TransactionRequest transactionRequest;
-        try {
-            transactionRequest = om.readValue(message, TransactionRequest.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(transactionRequest.toString());
-    }
+//    @KafkaListener(topics = "transactions")
+//    public void consume(String message){
+//        System.out.println("Kafka Consumer\n"+message);
+//        ObjectMapper om = new ObjectMapper();
+//        TransactionRequest transactionRequest;
+//        try {
+//            transactionRequest = om.readValue(message, TransactionRequest.class);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println(transactionRequest.toString());
+//    }
 
 }
 
