0f0026d0dd2bf08e7b0bfff523eaca571da0a320
Abdulhakim Edao <abdulhakim.edao1@gmail.com>
2023-04-23 22:55:34 -0500

    application.yml configured for gateway

--- src/main/java/com/order/OrderApplication.java
+++ src/main/java/com/order/OrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @EnableFeignClients
 @Import(CorsConfig.class)
+@EnableDiscoveryClient
 public class OrderApplication implements CommandLineRunner {
     @Autowired
     GetVendorService vendorService;
