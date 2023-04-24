838759f1af3d47448543a423ee0260476eb3ba58
arda009 <arda.agcal@gmail.com>
2023-04-23 22:32:41 -0500

    report cors update

--- src/main/java/com/example/reports/controller/TransactionController.java
+++ src/main/java/com/example/reports/controller/TransactionController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/transaction")
 public class TransactionController {
     private final TransactionNotificationService transactionNotificationService;
 
