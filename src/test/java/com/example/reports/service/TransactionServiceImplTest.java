28efb08dfff1a7befbf7ef882f072b129fde5a29
arda009 <arda.agcal@gmail.com>
2023-04-24 10:47:29 -0500

    report tests

--- src/test/java/com/example/reports/service/TransactionServiceImplTest.java
+++ src/test/java/com/example/reports/service/TransactionServiceImplTest.java
@@ -25,9 +25,8 @@ class TransactionServiceImplTest {
     TransactionRepository transactionRepository;
 
     @Test
-    public void createTransactionTest(){
+    public void createTransactionTest1(){
         Transaction transaction = Transaction.builder()
-                .transactionId(1)
                 .date(new Date())
                 .userId(1)
                 .userName("Arda")
@@ -107,7 +106,7 @@ class TransactionServiceImplTest {
                 .date(new Date())
                 .userId(2)
                 .userName("Kaan")
-                .transactionAmount(BigDecimal.valueOf(3000))
+                .transactionAmount(BigDecimal.valueOf(6000))
                 .build();
 
         ProductSales productSales = ProductSales.builder()
@@ -132,11 +131,100 @@ class TransactionServiceImplTest {
                 .vendorName("Samsung")
                 .total(1000)
                 .build();
+        ProductSales productSales3 = ProductSales.builder()
+                .transaction(transaction)
+                .productId(4)
+                .productName("Iphone")
+                .category("Electronics")
+                .pricePerUnit(1000)
+                .quantity(3)
+                .vendorId(1)
+                .vendorName("Samsung")
+                .total(3000)
+                .build();
+
+        transaction.setProductSales(List.of(productSales, productSales2, productSales3));
+
+        transactionServiceImpl.createTransaction(transaction);
+
+    }
+
+    @Test
+    public void createTransactionTest4(){
+        Transaction transaction = Transaction.builder()
+                .date(new Date())
+                .userId(3)
+                .userName("Mehmet Alp")
+                .transactionAmount(BigDecimal.valueOf(2000))
+                .build();
+
+        ProductSales productSales = ProductSales.builder()
+                .transaction(transaction)
+                .productId(1)
+                .productName("Macbook")
+                .category("Electronics")
+                .pricePerUnit(1000)
+                .quantity(1)
+                .vendorId(1)
+                .vendorName("Apple")
+                .total(1000)
+                .build();
+        ProductSales productSales2 = ProductSales.builder()
+                .transaction(transaction)
+                .productId(3)
+                .productName("Galaxy S20")
+                .category("Electronics")
+                .pricePerUnit(1000)
+                .quantity(1)
+                .vendorId(2)
+                .vendorName("Samsung")
+                .total(1000)
+                .build();
 
         transaction.setProductSales(List.of(productSales, productSales2));
 
         transactionServiceImpl.createTransaction(transaction);
 
     }
+
+    @Test
+    public void createTransactionTest5(){
+        Transaction transaction = Transaction.builder()
+                .date(new Date())
+                .userId(1)
+                .userName("Arda")
+                .transactionAmount(BigDecimal.valueOf(2000))
+                .build();
+
+        ProductSales productSales = ProductSales.builder()
+                .transaction(transaction)
+                .productId(1)
+                .productName("Macbook")
+                .category("Electronics")
+                .pricePerUnit(1000)
+                .quantity(1)
+                .vendorId(1)
+                .vendorName("Apple")
+                .total(1000)
+                .build();
+        ProductSales productSales2 = ProductSales.builder()
+                .transaction(transaction)
+                .productId(3)
+                .productName("Galaxy S20")
+                .category("Electronics")
+                .pricePerUnit(1000)
+                .quantity(1)
+                .vendorId(2)
+                .vendorName("Samsung")
+                .total(1000)
+                .build();
+
+        transaction.setProductSales(List.of(productSales, productSales2));
+
+        transactionServiceImpl.createTransaction(transaction);
+
+    }
+
+
 
 }
