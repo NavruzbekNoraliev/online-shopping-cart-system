d85f98d53d563494db4cdc5747f03b985b64b6cb
arda009 <arda.agcal@gmail.com>
2023-04-23 22:24:19 -0500

    report cors update

--- src/main/java/com/example/reports/ReportsApplication.java
+++ src/main/java/com/example/reports/ReportsApplication.java
@@ -1,11 +1,14 @@
 package com.example.reports;
 
+import com.example.reports.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(CorsConfig.class)
 public class ReportsApplication {
 
 	public static void main(String[] args) {
