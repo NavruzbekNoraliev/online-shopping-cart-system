79033dd2dd0fbef2d12583d07dba466a1808e3eb
Abdulhakim Edao <abdulhakim.edao1@gmail.com>
2023-05-04 21:13:00 -0500

    Minor Bug Fix

--- src/main/java/com/adminmodule/AdminModuleApplication.java
+++ src/main/java/com/adminmodule/AdminModuleApplication.java
@@ -2,10 +2,10 @@ package com.adminmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class  AdminModuleApplication {
 
 	public static void main(String[] args) {
